33fc49f5ee7a8fe525fc3c4a55476dd22720dc35
Alan <elanworld@163.com>
2022-06-15 17:57:16 +0800

    ws更改为wss

--- v2/service.ts
+++ v2/service.ts
@@ -2,6 +2,6 @@ let env = import.meta.env;
 
 
 export default {
-    wsPath: env.MODE === "development" ? 'ws://localhost:8030/entity' : 'ws://www.xianneng.top:8030/entity'
+    wsPath: env.MODE === "development" ? 'ws://localhost:8030/entity' : 'wss://www.xianneng.top/entity'
 }
 
